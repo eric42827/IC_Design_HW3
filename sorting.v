b9f6ba7127117857f4f61c2fbce7174ea5375556
eric42827 <b06502147@ntu.edu.tw>
2020-12-15 19:03:24 +0800

    hw template

---
+++ sorting.v
@@ -0,0 +1,10 @@
+`timescale 1ns/1ps
+
+module sorting(rank0, rank1, rank2, rank3, rank4, i0, i1, i2, i3, i4);
+//DO NOT CHANGE!
+	input  [5:0] i0, i1, i2, i3, i4;
+	output [5:0] rank0, rank1, rank2, rank3, rank4;
+//---------------------------------------------------
+
+
+endmodule
